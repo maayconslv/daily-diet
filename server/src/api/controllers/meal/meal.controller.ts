328d41aa2490e789a9f3bf537404ae2a52454246
mayconslv <mayconslv.contato@gmail.com>
2025-03-24 01:26:51 -0300

    refactor: fix controller name

--- server/src/api/controllers/meal/meal.controller.ts
+++ server/src/api/controllers/meal/meal.controller.ts
@@ -1,20 +1,20 @@
 import { Request } from "express";
 import { RegisterMealUseCase } from "../../../domain/meal";
 import { AuthMiddleware } from "../../middleware/authenticate.middleware";
-import { Body, Controller, Post, Req, UseBefore } from "routing-controllers";
+import { Body, Controller, Post, Put, Req, UseBefore } from "routing-controllers";
 import { Service } from "typedi";
 import { RegisterMealInput } from "./meal.input";
 
 @Service()
 @Controller('/meal')
-export class UserController {
+export class MealController {
   constructor(
     private readonly registerMealUseCase: RegisterMealUseCase,
   ) {}
 
   @Post()
   @UseBefore(AuthMiddleware)
-  async createUser(@Body() args: RegisterMealInput, @Req() req: Request) {
+  async registerMeal(@Body() args: RegisterMealInput, @Req() req: Request) {
     const response = await this.registerMealUseCase.exec({...args, userId: req.userId });
     return { data: response };
   }
